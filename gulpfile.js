3a59aa24c06683bdaeeae37b6fe8f1d433987b5e
i-tea <i-tea@email.com>
2017-04-06 20:54:56 +0200

    gulp-concat task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-    gulpUtil = require('gulp-util');
+    util = require('gulp-util'),
+    concat = require('gulp-concat');
+
+var jsSrc = ['dev/scripts/*.js'];
 
 gulp.task('log', function () {
-    gulpUtil.log('Starting project workflow');
+    util.log('Starting project workflow');
+});
+
+gulp.task('js', function () {
+    gulp.src(jsSrc)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('assets/js'))
 });
