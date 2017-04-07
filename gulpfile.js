b8a66c04c3e1429e8dbf35e3113152091115a4ce
i-tea <i-tea@email.com>
2017-04-07 19:03:04 +0200

    added compass and compass settings, need to fix problem with windows path for ruby

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
     util = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSrc = ['dev/scripts/*.js'];
+var sassSrc = ['dev/sass/style.scss'];
 
 gulp.task('log', function () {
     util.log('Starting project workflow');
@@ -12,5 +14,18 @@ gulp.task('log', function () {
 gulp.task('js', function () {
     gulp.src(jsSrc)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('assets/js'))
+});
+
+gulp.task('compass', function () {
+    gulp.src(sassSrc)
+        .pipe(compass({
+            css: 'assets/css',
+            sass: 'dev/sass',
+            style: 'compressed', // can be nested, expanded, compact and compressed
+            image: 'dev/img'
+        }))
+        //.on('error', gutil.log)
+        .pipe(gulp.dest('assets/css'));
 });
